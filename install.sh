92896b782f7252963c0fded65f3c7d036175f39f
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-01-08 16:06:15 -0600

    fix install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 declare -A files=(
-    ["~/.dotfiles/.bashrc"]="$HOME/.bashrc"
-    ["~/.dotfiles/git/.gitconfig"]="$HOME/.gitconfig"
-    ["~/.dotfiles/vim/.vimrc"]="$HOME/.vimrc"
+    [~/.dotfiles/.bashrc]="$HOME/.bashrc"
+    [~/.dotfiles/git/.gitconfig]="$HOME/.gitconfig"
+    [~/.dotfiles/vim/.vimrc]="$HOME/.vimrc"
     # Add more files in the same manner if needed
 )
 
@@ -11,9 +11,9 @@ for source_file in "${!files[@]}"; do
     target_file="${files[$source_file]}"
 
     if [ ! -L "$target_file" ]; then
-        ln -s "$source_file" "$target_file"
+        ln -s $source_file $target_file
     fi
 done
 
-source ~/.bashrc
+exec bash
 
