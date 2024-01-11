92530dde94d86aa64093dfa41160e4a42444bced
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-01-10 19:33:18 -0600

    add plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,10 @@
 return {
     -- { 'jiangmiao/auto-pairs' },
+    { 'f-person/git-blame.nvim' },
+    { 'nvim-tree/nvim-web-devicons' },
+    { 'nvim-tree/nvim-tree.lua' },
     { 'tpope/vim-commentary' },
     { 'tpope/vim-surround' },
     { 'wellle/targets.vim' }
 }
+
