374f0326bbfc39d9f2998e3c4101d424b410c742
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-01-16 22:40:07 -0600

    add copilot

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
     -- { 'jiangmiao/auto-pairs' },
+    { 'github/copilot.vim' },
     { 'f-person/git-blame.nvim' },
     { 'nvim-tree/nvim-web-devicons' },
     { 'nvim-tree/nvim-tree.lua' },
