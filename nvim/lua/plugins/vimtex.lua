d33cd088d44ffbc08c8c0ee4ce8aed661ba09ab5
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-01-25 12:38:29 -0600

    update vimteX

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -8,6 +8,21 @@ return {
         vim.g.vimtex_quickfix_mode = 0
         vim.opt.conceallevel = 2
         vim.g.tex_conceal = 'abdmgs'
+        vim.g.vimtex_compiler_latexmk = {
+            aux_dir = './aux',
+            out_dir = './out',
+            callback = 1,
+            continuous = 1,
+            executable = 'latexmk',
+            hooks = {},
+            options = {
+                '-verbose',
+                '-file-line-error',
+                '-synctex=1',
+                '-interaction=nonstopmode',
+                '-shell-escape',
+            },
+        }
     end
 }
 
