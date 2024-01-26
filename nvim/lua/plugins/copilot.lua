592ebe5ae7cb0672c9297e713e8fb8963b584cd2
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-01-26 12:09:39 -0600

    update copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,11 +1,11 @@
 return {
     'github/copilot.vim',
     config = function()
-        vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
           expr = true,
           replace_keycodes = false
         })
-        vim.g.copilot_no_tab_map = true
+        vim.g.copilot_no_tab_map = false
     end
 }
 
