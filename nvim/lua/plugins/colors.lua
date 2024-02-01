82f4623a65d6c8f44df98acf2f1118291f7ac068
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2024-02-01 17:07:05 -0600

    change color scheme to vscode

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,11 +1,10 @@
 return {
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    'Mofiqul/vscode.nvim',
     config = function()
-        require('rose-pine').setup({
+        require('vscode').setup({
             disable_background = true
         })
-        vim.cmd('colorscheme rose-pine')
+        vim.cmd('colorscheme vscode')
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
