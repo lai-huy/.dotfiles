c486f96c88b5634c2bc4cfef0bf0e3b54a16eba5
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2025-06-27 13:11:43 -0500

    rename codium to windsurf

--- nvim/lua/plugins/windsurf.lua
+++ nvim/lua/plugins/windsurf.lua
@@ -1,5 +1,9 @@
 return {
-    "Exafunction/codeium.vim",
+    "Exafunction/windsurf.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
     config = function ()
         -- Change '<C-g>' here to any keycode you like.
         vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
