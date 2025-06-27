d99deb50dc02477cfcc53f39f2092c76c21faf83
Huy Lai <98334390+lai-huy@users.noreply.github.com>
2025-06-27 12:56:45 -0500

    add nvim 11 autocomplete

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -60,5 +60,13 @@ return {
                 keybind(bufnr)
             end
         }
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(ev)
+                local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                if client:supports_method('textDocument/completion') then
+                    vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+                end
+            end,
+        })
     end,
 }
